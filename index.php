8d9b924005b0a8116dc111b177fad70274f514be
Nemolite <g25092011@mail.ru>
2021-11-23 12:03:40 +0300

    Адаптация изображения

--- index.php
+++ index.php
@@ -7,17 +7,75 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Flex</title>
 </head>
-<body>
-    <div class="box">
-        <div class="box-item">            
-                <p class="text-inner">1</p>           
+<body>     
+    <div class="box-picture">
+        <h1 class="box-picture__title">Адаптация изображения</h1>
+        <div class="box__text">
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
         </div>
-        <div class="box-item">       
-                <p class="text-inner">2</p>                
+        <div class="box__img">
+            <img src="img/img800x400.jpg" alt="">
         </div>
-        <div class="box-item">           
-                <p class="text-inner">3</p>            
+        <div class="box__text">
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
         </div>
-    </div>    
+        <div class="box__img">
+            <img src="img/img400x600.jpg" alt="">
+        </div>
+        <div class="box__text">
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+        </div>
+        <div class="box__img">
+            <img src="img/img1300x400.jpg" alt="">
+        </div>
+        <div class="box__text">
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+            <p class="text__inner">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam? Quidem aut cupiditate culpa, fugit atque sit laboriosam corrupti quaerat incidunt. Dignissimos quia reiciendis, labore architecto aliquid delectus consectetur corrupti sit omnis illo voluptates optio earum a error velit aliquam.
+            </p>
+        </div>
+
+    </div>
 </body>
 </html>
